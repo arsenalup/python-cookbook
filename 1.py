6df47c784d1f89a2df93859dcf26d51c29e8f153
chen keyan <676250063@qq.com>
2017-08-17 22:45:19 +0800

    8.17

--- 1.py
+++ 1.py
@@ -1,9 +1,28 @@
-from collections import deque
+# from collections import deque
+#
+#
+# def search(lines, pattern, history):
+#     previous_lines = deque(maxlen=history)
+#     for i in lines:
+#         if pattern in li:
+#             yield li, previous_lines
+#         previous_lines.append(li)
 
+#查找最大最小的n个元素
+# import heapq
+# nums = [1,2,3,4,5,6,7]
+# print(heapq.nlargest(3, nums))
+# print(heapq.nsmallest(3, nums))
 
-def search(lines, pattern, history):
-    previous_lines = deque(maxlen=history)
-    for i in lines:
-        if pattern in li:
-            yield li, previous_lines
-        previous_lines.append(li)
+# portfolio = [ {'name': 'IBM', 'shares': 100, 'price': 91.1},
+#               {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+#               {'name': 'FB', 'shares': 200, 'price': 21.09},
+#               {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+#               {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+#               {'name': 'ACME', 'shares': 75, 'price': 115.65}
+#               ]
+# cheap = heapq.nsmallest(3, portfolio, lambda s : s['price'])
+# # print(cheap)
+# expensive = heapq.nlargest(3, portfolio, lambda s : s['price'])
+# heapq.heapify(nums)
+# print(heapq.heappop(nums))
