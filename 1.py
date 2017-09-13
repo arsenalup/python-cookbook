b1b9976e7bbc5111c29996062b02d17db8804a10
chen keyan <676250063@qq.com>
2017-09-13 22:30:53 +0800

    9.13

--- 1.py
+++ 1.py
@@ -206,3 +206,11 @@
 #         return False
 # ivals = list(filter(is_int, values))
 # print(ivals)
+
+#1.17 从字典中提取子集
+# prices = { 'ACME': 45.23, 'AAPL': 612.78, 'IBM': 205.55, 'HPQ': 37.20, 'FB': 10.75 }
+# # p1 = {key:value for key, value in prices.items() if value > 200}
+# p1 = dict((key,value) for key, value in prices.items() if value > 200)
+# print(p1)
+
+#1.18 映射名称到序列元素
